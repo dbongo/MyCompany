59e63098d7481d983f96f09cc72565b753311bbf
Alex <alex.kovalskiy@icloud.com>
2013-11-17 17:53:02 +0200

    Добавлен новый класс "Salary" - данный класс хранит данные о зарплатах сотрудникв.

---
+++ src/com/project/classes/Salary.java
@@ -0,0 +1,11 @@
+package com.project.classes;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Alex
+ * Date: 11/17/13
+ * Time: 5:52 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Salary {
+}
--- src/com/project/classes/Submission.java
+++ src/com/project/classes/Submission.java
@@ -1,11 +1,5 @@
 package com.project.classes;
 
-/**
- * Created with IntelliJ IDEA.
- * User: Alex
- * Date: 11/17/13
- * Time: 5:51 PM
- * To change this template use File | Settings | File Templates.
- */
+/* Данный класс хранит данные о подчиненных. */
 public class Submission {
 }
