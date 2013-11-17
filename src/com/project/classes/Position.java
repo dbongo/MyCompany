41a35ee20bcd372ef59c581f894734ac634f1922
Alex <alex.kovalskiy@icloud.com>
2013-11-17 17:47:18 +0200

    Добавлен новый класс "Position" - класс будет хранить должности сотрудников.

---
+++ src/com/project/classes/Position.java
@@ -0,0 +1,11 @@
+package com.project.classes;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Alex
+ * Date: 11/17/13
+ * Time: 5:46 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Position {
+}
